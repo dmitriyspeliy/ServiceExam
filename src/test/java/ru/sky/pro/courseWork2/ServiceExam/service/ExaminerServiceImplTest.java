4c61b4ba4040f7fe6e211ceb3c8be8de79be81bd
dmitryspeliy <dmitriypospelov93@gmail.com>
2022-10-26 14:55:12 +0700

    TestExam

--- src/test/java/ru/sky/pro/courseWork2/ServiceExam/service/ExaminerServiceImplTest.java
+++ src/test/java/ru/sky/pro/courseWork2/ServiceExam/service/ExaminerServiceImplTest.java
@@ -1,37 +1,41 @@
 package ru.sky.pro.courseWork2.ServiceExam.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import ru.sky.pro.courseWork2.ServiceExam.entity.Question;
 import ru.sky.pro.courseWork2.ServiceExam.repository.JavaQuestionRepository;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ExaminerServiceImplTest {
 
-
     @Mock
-    ExaminerServiceImpl examinerService;
+    List<QuestionService> questions;
 
-    Set<Question> questionTest = new HashSet<>();
+    @InjectMocks
+    ExaminerServiceImpl examinerService;
 
 
     @Test
     void getQuestion() {
-        questionTest.add(new Question("1 + 1 =","2"));
-        questionTest.add(new Question("Java","1"));
-        lenient().when(examinerService.getQuestion()).thenReturn(questionTest);
-        //как проверить этот метод...я не знаю
+        JavaQuestionRepository javaQuestionRepository = new JavaQuestionRepository();
+        javaQuestionRepository.add(new Question("Java", "1"));
+        when(questions.get(0)).thenReturn(new JavaServiceImpl(javaQuestionRepository));
+        when(questions.get(1)).thenReturn(new MathQuestionService());
+        assertEquals(2,examinerService.getQuestion().size());
 
     }
 
